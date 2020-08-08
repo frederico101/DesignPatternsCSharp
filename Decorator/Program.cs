319d4d20bb917a885e0bc11e6c7e5d80c99b8772
frederico101 <fredyalves.fredyalves@hotmail.com>
2020-08-08 11:21:02 -0300

    Estudando desing patterns

---
+++ Decorator/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+
+namespace Design
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //decortor desing pattern
+            Ipizza pizzaPequena = new Pizza(); 
+            Ipizza pizzaMedia = new Pizza();
+            Ipizza ComBordas = new BordasDecorator(pizzaMedia);
+            Ipizza cheese = new CheeseDecorator(pizzaPequena);
+            Ipizza frango = new FrangoDecorator(cheese);
+            Ipizza palmito = new PalmitoDecorator(frango);
+            Console.Write(palmito.PizzaPequena());
+            Console.WriteLine( cheese.PizzaMedia());
+            Console.WriteLine(pizzaMedia.PizzaMedia());
+            Console.WriteLine(ComBordas.PizzaMedia());  // como vou saber se é com bordas ou sem ????
+        }
+    }
+    // base interface 
+    interface Ipizza
+    {
+        string PizzaPequena();
+        string PizzaMedia();
+    }
+    //concrete class 
+    
+    class Pizza : Ipizza
+    {
+        public string PizzaPequena()
+        {
+            return "Pizza pequena\n";
+        }
+
+        public string PizzaMedia()
+        {
+            return "Pizza media\n";
+        }
+    }
+    //base decorator class
+    class PizzaDecorator : Ipizza
+    {
+        private Ipizza _pizza;
+        public PizzaDecorator(Ipizza pizza)
+        {
+            _pizza = pizza;
+        }
+        public virtual string PizzaPequena()
+        {
+           return _pizza.PizzaPequena();
+        }
+        public virtual string PizzaMedia()
+        {
+             return _pizza.PizzaMedia();
+        }
+
+    }
+    //derived decorator class 
+    
+     class BordasDecorator : PizzaDecorator
+    {
+        public BordasDecorator(Ipizza pizza): base(pizza){}
+
+        public override string PizzaPequena()
+        {
+            string type = base.PizzaPequena();
+            type += " Com bordas\n";
+            return type;
+        }
+        public override string PizzaMedia()
+        {
+            string type = base.PizzaMedia();
+            type += " Com bordas";
+            return type;
+        }
+
+        
+    }
+
+
+    class CheeseDecorator : PizzaDecorator
+    {
+        public CheeseDecorator(Ipizza pizza): base(pizza){}
+
+        public override string PizzaPequena()
+        {
+            string type = base.PizzaPequena();
+            type += " Com queijo extra\n";
+            return type;
+        }
+        public override string PizzaMedia()
+        {
+            string type = base.PizzaMedia();
+            type += " Com queijo extra e bordas";
+            return type;
+        }
+
+        
+    }
+
+    class FrangoDecorator : PizzaDecorator
+    {
+        public FrangoDecorator(Ipizza pizza): base(pizza){}
+
+         public override string PizzaPequena()
+        {
+            string type = base.PizzaPequena();
+            type += " Com Frango extra\n";
+            return type;
+        }
+        
+    }
+     class PalmitoDecorator : PizzaDecorator
+    {
+        public PalmitoDecorator(Ipizza pizza): base(pizza){}
+
+         public override string PizzaPequena()
+        {
+            string type = base.PizzaPequena();
+            type += " Com Palmito extra\n";
+            return type;
+        }
+        
+    }
+    
+}
---
+++ Padaria/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Padaria
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPao manteiga = new ManteigaDecorator(new Pao());
+            Console.WriteLine(manteiga.PaoFrances());
+        }
+    }
+
+    interface IPao
+    {
+        string PaoFrances();
+    }
+    class Pao : IPao
+    {
+        public string PaoFrances()
+         => "Pão Frances\n";
+    }
+    class PaoDecorator : IPao
+    {
+        private IPao _ipao;
+        public PaoDecorator(IPao pao)
+        {
+            _ipao = pao;
+        }
+        public virtual string PaoFrances()
+         =>_ipao.PaoFrances();
+    }
+
+    class ManteigaDecorator : PaoDecorator
+    {
+        public ManteigaDecorator(IPao pao) : base(pao){}
+
+        public override string PaoFrances()
+        {
+             var paoFrances = base.PaoFrances();
+             paoFrances += "Com manteiga";
+             return paoFrances;
+        }
+
+    }
+}
